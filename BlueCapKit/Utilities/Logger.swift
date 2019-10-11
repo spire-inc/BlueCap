c990d11d9af1c334b879c6756800eb0b377193ed
Pawel Chmiel <pawel.chmiel@tooploox.com>
2019-10-11 10:00:31 +0200

    get rid of debug flag and set default value for logging enabled to false

--- BlueCapKit/Utilities/Logger.swift
+++ BlueCapKit/Utilities/Logger.swift
@@ -10,17 +10,15 @@ import Foundation
 
 public class Logger {
     
-    public static var debugLoggingEnabled = true
+    public static var loggingEnabled = false
     
     public class func debug(_ message:String? = nil, function: String = #function, file: String = #file, line: Int = #line) {
-        #if DEBUG
-        guard debugLoggingEnabled else { return }
+        guard loggingEnabled else { return }
         
         if let message = message {
             print("\(file):\(function):\(line): \(message)")
         } else {
             print("\(file):\(function):\(line)")
         }
-        #endif
     }
 }
